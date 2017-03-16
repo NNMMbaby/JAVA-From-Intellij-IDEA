387b6df4cce41d4e62963fbbd56b7c63d06d8526
Jessica <Lynn@192.168.16.153>
2017-03-16 15:10:02 +0800

    初始化

---
+++ src/com/Lynn/Home/Person.java
@@ -0,0 +1,15 @@
+package com.Lynn.Home;
+
+/**
+ * Created by Lynn on 17/3/16.
+ */
+public class Person {
+
+    public static void main(String[] args){
+        System.out.print("Hello World");
+
+    }
+
+
+
+}
